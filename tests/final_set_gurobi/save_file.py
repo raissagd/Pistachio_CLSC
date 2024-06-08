98b11b3a7adbe68fe2329de1a6c96984b75ad0db
raissagd <raissagdiniz@gmail.com>
2024-06-08 15:46:22 -0300

    Solutions

---
+++ tests/final_set_gurobi/save_file.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+best_objective = 70598781.54841062
+np.savez('solution_100.npz', solutions=best_objective)
--- tests/final_set_gurobi/script_800.py
+++ tests/final_set_gurobi/script_800.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append(r'C:\Users\Lenovo\Documents\IC')
+sys.path.append(r'C:\Users\Acer\Documents\IC')  
 from Classes.Problem import Problem
 from Classes.Algorithm import ExactAlgorithm
 import numpy as np
@@ -8,8 +8,8 @@ data = Problem()
 # data_size = [100, 200, 400, 800, 1600]
 solutions = []
 
-data.loadFile("data/data_400.npz")
+data.loadFile("data/data_800.npz")
 gurobi = ExactAlgorithm()
 solution = gurobi.solve(data)
 solutions.append(solution)
-np.savez('tests/final_set_gurobi/solution_400.npz', solutions=solutions)
+np.savez('tests/final_set_gurobi/solution_800.npz', solutions=solutions)
