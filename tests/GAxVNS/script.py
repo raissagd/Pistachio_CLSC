9be6295d34c7b34d0d802e09fcaff09c2b3ece86
raissagd <raissagdiniz@gmail.com>
2024-06-23 16:31:12 -0300

    Fix: GA vs VNS

--- tests/GAxVNS/script.py
+++ tests/GAxVNS/script.py
@@ -5,7 +5,7 @@ from Classes.Algorithm import GeneticAlgorithm
 import numpy as np
 
 data = Problem()
-data.loadFile("data/data_10.npz")
+data.loadFile("data/data_100.npz")
 
 # Genetic Algorithm parameters
 population_size = 100
@@ -31,4 +31,4 @@ for i in range(30):
     best_solutions.append(best_solution.FX)
     print(f"Iteration {i+1}: Best solution found: {best_solution.FX}")
 
-np.savez('solutions_10_GA.npz', best_solutions=best_solutions)
+np.savez('solutions_100_GA.npz', best_solutions=best_solutions)
