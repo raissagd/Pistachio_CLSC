363aca078fef79a95703484141875e9f1ec83b44
André Batista <wmwandre@gmail.com>
2024-07-11 11:41:21 -0300

    Adicionei a rotina pra salvar múltiplas soluções

--- Classes/Persistence.py
+++ Classes/Persistence.py
@@ -1,6 +1,6 @@
 import pickle
 
-class SaveSolution:
+class PersistSingleSolution:
     def __init__(self) -> None:
         self.format = '.pkl'
     def save(self, solution=None, filename='', filepath=''):
@@ -10,3 +10,48 @@ class SaveSolution:
         with open(filepath + filename + self.format, 'rb') as file:
             solution = pickle.load(file)
         return solution
+
+class PersistMultipleSolutions:
+    """
+    A class for persisting multiple solutions using pickle serialization.
+
+    Attributes:
+        format (str): The file format for serialization.
+
+    Methods:
+        save: Save the solutions to a file.
+        load: Load the solutions from a file.
+    """
+
+    def __init__(self) -> None:
+        self.format = '.pkl'
+
+    def save(self, solutions=None, filename='', filepath=''):
+        """
+        Save the solutions to a file.
+
+        Args:
+            solutions (object): The solutions to be saved.
+            filename (str): The name of the file.
+            filepath (str): The path to the file.
+
+        Returns:
+            None
+        """
+        with open(filepath + filename + self.format, 'wb') as file:
+            pickle.dump(solutions, file)
+
+    def load(self, filename='', filepath=''):
+        """
+        Load the solutions from a file.
+
+        Args:
+            filename (str): The name of the file.
+            filepath (str): The path to the file.
+
+        Returns:
+            object: The loaded solutions.
+        """
+        with open(filepath + filename + self.format, 'rb') as file:
+            solutions = pickle.load(file)
+        return solutions
--- Classes/Persistence_test.py
+++ Classes/Persistence_test.py
@@ -1,9 +1,10 @@
 import unittest
-from Persistence import SaveSolution
+import os
+from Persistence import PersistSingleSolution, PersistMultipleSolutions
 from Solution import Solution
 from Convergence import Convergence
 
-class SaveSolutionTests(unittest.TestCase):
+class PersistSingleSolutionTests(unittest.TestCase):
     def setUp(self):
         self.solution = Solution()
         self.solution.FX = 1.0
@@ -20,7 +21,7 @@ class SaveSolutionTests(unittest.TestCase):
 
     def test_save_and_load(self):
         # Create an instance of SaveSolution
-        save_solution = SaveSolution()
+        save_solution = PersistSingleSolution()
 
         # Save the solution
         save_solution.save(self.solution, self.filename)
@@ -33,13 +34,44 @@ class SaveSolutionTests(unittest.TestCase):
 
     def test_load_with_filepath(self):
         # Create an instance of SaveSolution
-        save_solution = SaveSolution()
+        save_solution = PersistSingleSolution()
 
         # Load the solution with filepath
         loaded_solution = save_solution.load(self.filename, self.filepath)
 
         # Assert that the loaded solution is equal to the original solution
         self.assertEqual(loaded_solution, self.solution)
+
+class TestPersistMultipleSolutions(unittest.TestCase):
+    def setUp(self):
+        self.persistence = PersistMultipleSolutions()
+        self.filename = 'test_solutions'
+        self.filepath = './'
+        self.solutions = []
+        for i in range(3):
+            solution = Solution()
+            solution.FX = i
+            for j in range(1, 9):
+                setattr(solution, f'S{j}', [j])
+            attributes = ['X', 'Go', 'Gr', 'Gw', 'O', 'Oc', 'Ow', 'L', 'P', 'D',
+                          'U', 'Y', 'W', 'R', 'V']
+            for j, attr in enumerate(attributes, 1):
+                setattr(solution, attr, [j])
+            self.solutions.append(solution)
+
+
+    def tearDown(self):
+        # Clean up the test file after each test
+        file_path = self.filepath + self.filename + self.persistence.format
+        if os.path.exists(file_path):
+            os.remove(file_path)
+
+    def test_save_and_load(self):
+        # Test saving and loading solutions
+        solutions = ['solution1', 'solution2', 'solution3']
+        self.persistence.save(solutions, self.filename, self.filepath)
+        loaded_solutions = self.persistence.load(self.filename, self.filepath)
+        self.assertEqual(solutions, loaded_solutions)
 
 if __name__ == '__main__':
     unittest.main()
