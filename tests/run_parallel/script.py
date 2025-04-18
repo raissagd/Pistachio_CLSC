b2b0ebaa96c8b0d83727b31d872b80ff592b2599
raissagd <raissagdiniz@gmail.com>
2025-04-18 16:22:45 -0300

    Valores para o problema de tamanho 400

--- tests/run_parallel/script.py
+++ tests/run_parallel/script.py
@@ -7,15 +7,15 @@ from Execute import RunMultipleMethodsMultipleTimes
 from Persistence import PersistMultipleSolutions
 
 number_evaluations = 100000
-vns1 = VariableNeighborhoodSearch([Swap(2), Reversion(2), Insertion(2), Slide(2)], number_evaluations, 1)
-vns2 = VariableNeighborhoodSearch([ETN(2), RS(2), SPS(2), SRPS(2)], number_evaluations, 1)
-vns3 = VariableNeighborhoodSearch([Swap(2), Reversion(2), ETN(2), RS(2)], number_evaluations, 1)
+vns1 = VariableNeighborhoodSearch([Swap(2), Reversion(2), Insertion(2), Slide(2)], number_evaluations, 1, name='VNS_Swap')
+vns2 = VariableNeighborhoodSearch([ETN(2), RS(2), SPS(2), SRPS(2)], number_evaluations, 1, name='VNS_ETN')
+vns3 = VariableNeighborhoodSearch([Swap(2), Reversion(2), ETN(2), RS(2)], number_evaluations, 1, name='VNS_SwapETN')
 methods = [vns1, vns2, vns3]
 number_executions = 30
 
 data = Problem()
-data.loadFile("data/data_1600.npz")
+data.loadFile("data/data_10.npz")
 
 results = RunMultipleMethodsMultipleTimes().run(data, methods, number_executions)
 
-PersistMultipleSolutions().save(results, 'script_1600_results', './tests/run_parallel/results/')
+PersistMultipleSolutions().save(results, 'teste', './tests/run_parallel/logs/')
