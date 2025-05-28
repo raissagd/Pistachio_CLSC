0c6c28d06f82acea76c35f110218af627e57dee3
raissagd <raissagdiniz@gmail.com>
2025-05-28 20:59:49 -0300

    Atualizações no artigo do congresso

--- tests/GAxVNS/graphs.py
+++ tests/GAxVNS/graphs.py
@@ -2,19 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Carregar os dados dos arquivos .npz
-ga_data = np.load('./tests/GAxVNS/results/solutions_1600_GA_FX.npz')
-vns_data = np.load('./tests/GAxVNS/results/solutions_1600_VNS.npz')
+ga_data = np.load('./tests/GAxVNS/new_results/GA_400_FX.npz')['fx_array']
+vns_data = np.load('./tests/run_parallel/new_results/script_400_fx_values.npz')['fx_array']
 
 # Extrair os arrays
-ga_fx = ga_data['fx_array']
-vns_fx = vns_data['fx_array']
+vns_fx = vns_data[:30]
 
 # Criar o boxplot
 plt.figure(figsize=(8, 6))
-plt.boxplot([ga_fx, vns_fx], labels=['GA', 'VNS'])
+plt.boxplot([ga_data, vns_fx], labels=['GA', 'VNS'], notch=True)
 
 # Adicionar título e rótulos
-plt.title('Comparison of Objective Function Values (Instance Size = 1600)')
+plt.title('Comparison of Objective Function Values (Instance Size = 400)')
 plt.ylabel('Objective Function Value')
 plt.grid(True)
 
